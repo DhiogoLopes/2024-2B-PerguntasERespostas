0639d55f1a35a821b4b8ca2fdac45fdcc065f70e
DhiogoLopes <141455193+DhiogoLopes@users.noreply.github.com>
2024-11-07 08:34:06 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criarCartao(
+    "filosofia",
+    "Quem disse a famosa frase 'eu sabo'?",
+    "Albert Einstein"
+)
+
+criarCartao(
+    "Historia",
+    "Quem descobriu o brasil?",
+    "A Dora Aventureira"
+)
+
+criarCartao(
+    "filosofia",
+    "Quem disse a famosa frase 'eu sabo'?pergunta",
+    "albert einstein"
+)
+
+criarCartao(
+    "filosofia",
+    "Quem disse a famosa frase 'eu sabo'?pergunta",
+    "albert einstein"
+)
+
+criarCartao(
+    "filosofia",
+    "Quem disse a famosa frase 'eu sabo'?pergunta",
+    "albert einstein"
+)
+
+criarCartao(
+    "filosofia",
+    "Quem disse a famosa frase 'eu sabo'?pergunta",
+    "albert einstein"
+)
